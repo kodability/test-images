b32abd4d13e93220d3c5f46e1665b689fdf66bc5
Lechuck Roh <member@lechuck2011.cafe24.com>
2018-03-25 00:29:24 +0900

    javascript test update

--- javascript/test/exampleSpec.js
+++ javascript/test/exampleSpec.js
@@ -3,7 +3,10 @@ const example = require('../src/example');
 const sum = example.sum;
 
 describe('', function() {
-    it('test', function() {
+    it('test1', function() {
         assert.equal(sum(1,10), 55);
+    });
+    it('test2', function() {
+        assert.equal(sum(1,1), 1);
     });
 });
